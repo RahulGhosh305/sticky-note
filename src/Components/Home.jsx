ab1f654ffd6b9b4802fed696c94a8954d9543737
Rahul Ghosh <rahulghosh096@gmail.com>
2021-08-09 23:24:48 +0600

    avg done with react but without fabric js

---
+++ src/Components/Home.jsx
@@ -0,0 +1,179 @@
+import React, { useState, useReducer } from 'react'
+import './App.css';
+import { v4 as uuid } from 'uuid'
+
+const initialState = {
+    lastNoteCreatedAt: null,
+    totalNotes: 0,
+    notes: []
+}
+
+const notesReducer = (prevState, action) => {
+    switch (action.type) {
+        case 'ADD_NOTE':
+            const newNote = {
+                lastNoteCreatedAt: new Date().toTimeString().slice(0, 8),
+                totalNotes: prevState.notes.length + 1,
+                notes: [...prevState.notes, action.payload]
+            }
+            return newNote;
+
+        case 'DELETE_NOTE':
+            const deleteNote = {
+                ...prevState,
+                totalNotes: prevState.notes.length - 1,
+                notes: prevState.notes.filter(note => note.id !== action.payload.id)
+            }
+            return deleteNote
+
+        default:
+            return prevState;
+    }
+}
+
+
+const Home = () => {
+    const [notesState, dispatch] = useReducer(notesReducer, initialState)
+    const [noteText, setNoteText] = useState('')
+    // const [show, setToggleShow] = useState(false)
+
+    const addNote = (e) => {
+        e.preventDefault();
+
+        if (!noteText) {
+            return;
+        }
+
+        const newNote = {
+            id: uuid(),
+            text: noteText,
+            rotate: Math.floor(Math.random() * 20)
+        }
+
+        dispatch({ type: 'ADD_NOTE', payload: newNote })
+        setNoteText('')
+        // setToggleShow(false)
+    }
+
+
+    const dropNote = (e) => {
+        e.target.style.left = `${e.pageX - 50}px`;
+        e.target.style.top = `${e.pageY - 50}px`;
+    }
+
+    const dragOver = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    const deleteNote = (id) => {
+        console.log('delete')
+        dispatch({ type: 'DELETE_NOTE', payload: id })
+    }
+
+    let i = 0
+    const randomColor = () => {
+        let random_color = ['#c2ff3d', '#04e022', '#fff740', '#ff3de8', '#3dc2ff', '#bc83e6']
+        if (i > random_color.length - 1) {
+            i = 0
+        }
+        console.log('ok');
+        return random_color[i++]
+    }
+
+
+
+    return (
+        <div className="App" onDragOver={dragOver}>
+            <div className="container">
+                <h1>Sticky Note</h1>
+
+                {/* <button onClick={() => setToggleShow(!show)} className="btn btn-success">Add New Note</button> */}
+
+
+
+                <div>
+
+
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#declineModal">
+                        Create Note
+                    </button>
+
+
+
+
+                    {/* {
+                        show && <div className="formBox">
+                            <form className="main-form" onSubmit={addNote}>
+                                <textarea placeholder="Write Note..."
+                                    value={noteText}
+                                    onChange={(e) => setNoteText(e.target.value)}
+                                ></textarea>
+
+                                <button id="x-icon" onClick={() => setToggleShow(!show)}></button>
+                                <button></button>
+                            </form>
+                        </div>
+                    } */}
+
+
+
+
+
+
+
+
+                    <div class="modal fade" id="declineModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Add New Note</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <form className="main-form" onSubmit={addNote}>
+                                        <textarea placeholder="Write Note..."
+                                            value={noteText}
+                                            onChange={(e) => setNoteText(e.target.value)}
+                                        ></textarea>
+
+                                        <button id="x-icon" type="button" class="btn btn-secondary" data-dismiss="modal"></button>
+                                        
+                                        <button></button>
+                                        
+                                    
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+
+                    <div className='display-note'>
+                        {notesState.notes.map((note) => (
+                            <div className="note"
+                                style={{ backgroundColor: `${randomColor()}`, transform: `rotate(${3}deg)` }}
+                                key={note.id}
+                                draggable="true"
+                                onDragEnd={dropNote}
+                            >
+                                <h4 className="text">{note.text}</h4>
+                                <button className="delete-btn" onClick={() => deleteNote(note)}>
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
